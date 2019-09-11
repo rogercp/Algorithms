1a9744d2f9652a3773364315229b9e2aff0d7440
Roger Caballero <rogercp1234@yahoo.com>
2019-09-11 15:26:21 -0700

    almost have recipe batches working

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,9 +2,28 @@
 
 import math
 
-def recipe_batches(recipe, ingredients):
-  pass 
 
+recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+ingredients = { 'milk': 131, 'butter': 48, 'flour': 3 }
+
+def recipe_batches(recipe, ingredients):
+	batches = None
+	if(recipe.keys() != ingredients.keys()):
+		return 0
+	else:
+		for i in recipe.items():
+			for j in ingredients.items():
+				if(j[1] < i[1]):
+					return 0
+				else:
+					result = j[1] / i[1]
+					if batches == None or result < batches:
+						batches = result
+	
+	return int(batches)
+
+
+recipe_batches(recipe,ingredients)
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
